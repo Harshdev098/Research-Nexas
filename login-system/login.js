a3a62106b281c626a7204ad1d4f2572ed1cb206d
rishi <rishivt24@gmail.com>
2024-10-04 23:45:18 +0530

    Fixed typos

--- login-system/login.js
+++ login-system/login.js
@@ -33,6 +33,7 @@ const signup=async (req, res) => {
         if (err) throw (err)
             const sqlSearch = "SELECT * FROM user_table WHERE email=? OR username=?";
         const search_query = mysql.format(sqlSearch, [email,username])
+        
         const sqlinsert = "INSERT INTO user_table VALUES (0,?,?,?)"
         const insert_query = mysql.format(sqlinsert, [username, email, hashpassword])
         await connection.query(search_query, async (err, result) => {
