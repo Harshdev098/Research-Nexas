c9381085a9752b4b9e2a6c4652126a71f1e235b5
harish08102004 <harishnik08@gmail.com>
2024-10-07 11:49:08 +0530

    Moved hardcoded credentials to .env file for better security and integration

--- file_upload/upload.js
+++ file_upload/upload.js
@@ -5,13 +5,21 @@ const destination = path.resolve(__dirname, 'uploads/');
 const {decodeAccessToken}=require('../login-system/token')
 const mysql = require('mysql')
 
+
+const DB_HOST = process.env.DB_HOST
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_DATABASE = process.env.DB_DATABASE
+const DB_PORT = process.env.DB_PORT
+
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'user',
-    password: 'Ha@96168737',
-    database: 'user_DB',
-    port: '3306'
- })
+    connectionLimit: 100,
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    port: DB_PORT
+})
 
 // uploading file 
 
