8e2ddd548ef698ac277619eb3bd70e904f7dd0ba
ygowthamr <ygowthamr@gmail.com>
2024-10-10 18:43:24 +0530

    Solved ''View the File link'' is not working for the stakeholder while allotment and approval of paper issue

--- file_upload/upload.js
+++ file_upload/upload.js
@@ -1,75 +1,74 @@
-const multer = require('multer')
-const path = require('path')
-const notify=require('../login-system/notification')
-const destination = path.resolve(__dirname, 'uploads/');
-const {decodeAccessToken}=require('../login-system/token')
-const mysql = require('mysql')
-const  db  = require('../config/mysql_connection')
-
+const multer = require("multer");
+const path = require("path");
+const notify = require("../login-system/notification");
+const destination = path.resolve(__dirname, "uploads/");
+const { decodeAccessToken } = require("../login-system/token");
+const mysql = require("mysql");
+const db = require("../config/mysql_connection");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, destination);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-    }
-})
+  destination: (req, file, cb) => {
+    cb(null, destination);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 const upload = multer({ storage: storage });
 
-// saving the file data in the database 
-const save = async (req,res) => {
-    const decodedToken = await decodeAccessToken(req.headers.authorization);
+// saving the file data in the database
+const save = async (req, res) => {
+  const decodedToken = await decodeAccessToken(req.headers.authorization);
+  if (!decodedToken || !decodedToken.user) {
+    console.error("Invalid or missing user information in the token");
+    return res.status(401).send("Unauthorized");
+  }
+  const userid = decodedToken.user;
+  const filename = req.file.filename;
+  const filepath = `/uploads/${filename}`;
+  db.getConnection(async (err, connection) => {
+    if (err) throw err;
+    const sql = "INSERT INTO upload_file_db VALUES(0,?,?,?,0,null)";
+    const query = mysql.format(sql, [userid, filename, filepath]);
+    await connection.query(query, async (err, result) => {
+      connection.release();
+      if (err) {
+        console.error("Error inserting file into database:", err);
+        return res.status(500).send("Internal Server Error");
+      }
+      console.log("Data Saved");
+      // const sub='Research Paper Uploaded'
+      // const content=`Your Research Paper ${result[0].filename} has been uploaded with username ${result[0].username}`
+      // notify(req,res,result[0].email,sub,content);
+    });
+  });
+};
+
+// displaying uploaded research papers to the client whom it belongs
+const disp = (req, res) => {
+  try {
+    const decodedToken = decodeAccessToken(req.headers.authorization);
     if (!decodedToken || !decodedToken.user) {
-        console.error('Invalid or missing user information in the token');
-        return res.status(401).send('Unauthorized');
+      console.error("Invalid or missing user information in the token");
+      return res.status(401).send("Unauthorized");
     }
     const userid = decodedToken.user;
-    const filename = req.file.filename;
-    const filepath = req.file.path;
-    db.getConnection(async(err, connection) => {
-        if (err) throw err;
-        const sql = "INSERT INTO upload_file_db VALUES(0,?,?,?,0,null)"
-        const query = mysql.format(sql, [userid, filename, filepath])
-        await connection.query(query,async (err, result) => {
-            connection.release();
-            if (err) {
-                console.error('Error inserting file into database:', err);
-                return res.status(500).send('Internal Server Error');
-            }
-            console.log('Data Saved');
-            // const sub='Research Paper Uploaded'
-            // const content=`Your Research Paper ${result[0].filename} has been uploaded with username ${result[0].username}`
-            // notify(req,res,result[0].email,sub,content);
-        });
-    })
-}
-
-// displaying uploaded research papers to the client whom it belongs 
-const disp = (req, res) => {
-    try {
-        const decodedToken = decodeAccessToken(req.headers.authorization);
-        if (!decodedToken || !decodedToken.user) {
-            console.error('Invalid or missing user information in the token');
-            return res.status(401).send('Unauthorized');
-        }
-        const userid = decodedToken.user;
-        const sqlquery = "SELECT filename FROM upload_file_db WHERE userid=?";
-        const query = mysql.format(sqlquery, [userid]);
-        db.query(query, (err, result) => {
-            if (err) {
-                console.error('Error querying database:', err);
-                return res.status(500).send('Internal Server Error');
-            }
-            const filenames = result.map(row => row.filename);
-            const filepaths=result.map(row => row.filename);
-            // console.log(filenames,filepaths);
-            res.json({ filenames,filepaths });
-        });
-    } catch (error) {
-        console.error('Error during disp:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    const sqlquery = "SELECT filename FROM upload_file_db WHERE userid=?";
+    const query = mysql.format(sqlquery, [userid]);
+    db.query(query, (err, result) => {
+      if (err) {
+        console.error("Error querying database:", err);
+        return res.status(500).send("Internal Server Error");
+      }
+      const filenames = result.map((row) => row.filename);
+      const filepaths = result.map((row) => row.filename);
+      // console.log(filenames,filepaths);
+      res.json({ filenames, filepaths });
+    });
+  } catch (error) {
+    console.error("Error during disp:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 // exporting upload
--- public/script/paper_allotment.js
+++ public/script/paper_allotment.js
@@ -1,141 +1,138 @@
-
-
 const fetchuploadfiles = async () => {
-    const token = localStorage.getItem('accessToken');
-    const headers = new Headers();
-    headers.append('Authorization', `Bearer ${token}`);
-    const response = await fetch('http://localhost:3000/allotment', {
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        },
-    });
+  const token = localStorage.getItem("accessToken");
+  const headers = new Headers();
+  headers.append("Authorization", `Bearer ${token}`);
+  const response = await fetch("http://localhost:3000/allotment", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
-    const results = await response.json();
-    const ul = document.getElementById('ul1');
-    if (Array.isArray(results)) {
-        results.forEach((result) => {
-            createListItem(result, ul);
-        });
-    } else {
-        createListItem(results, ul);
-    }
+  const results = await response.json();
+  const ul = document.getElementById("ul1");
+  if (Array.isArray(results)) {
+    results.forEach((result) => {
+      createListItem(result, ul);
+    });
+  } else {
+    createListItem(results, ul);
+  }
 };
 
 const createListItem = (result, ul) => {
-    const li = document.createElement('li');
-    const h4 = document.createElement('h4');
-    const img = document.createElement('img');
-    img.src = 'images/upload-image.jpg';
-    li.appendChild(img);
-    h4.textContent = result.filename;
-    li.appendChild(h4);
-    const strong = document.createElement('strong');
-    strong.textContent = result.name;
-    li.appendChild(strong);
+  const li = document.createElement("li");
+  const h4 = document.createElement("h4");
+  const img = document.createElement("img");
+  img.src = "images/upload-image.jpg";
+  li.appendChild(img);
+  h4.textContent = result.filename;
+  li.appendChild(h4);
+  const strong = document.createElement("strong");
+  strong.textContent = result.name;
+  li.appendChild(strong);
 
-    const p = document.createElement('p');
-    p.textContent = result.collegename;
-    li.appendChild(p);
-    const id = document.createElement('p')
-    id.textContent = result.id;
-    li.appendChild(id)
-    const btn = document.createElement('button');
-    btn.textContent = "View";
-    btn.type = "button";
-    if (result.status == 0) {
-        btn.disabled = true;
-        console.log("btn disable")
-    } else {
-        btn.disabled = false;
-        const container = document.getElementById('container')
-        btn.onclick = () => {
-            container.style.display = 'block'
-            display(result.id);
-            const papers = document.querySelector('.papers')
-            papers.style.opacity = '0.6'
-        }
-    }
-    li.appendChild(btn);
-    ul.appendChild(li);
+  const p = document.createElement("p");
+  p.textContent = result.collegename;
+  li.appendChild(p);
+  const id = document.createElement("p");
+  id.textContent = result.id;
+  li.appendChild(id);
+  const btn = document.createElement("button");
+  btn.textContent = "View";
+  btn.type = "button";
+  if (result.status == 0) {
+    btn.disabled = true;
+    console.log("btn disable");
+  } else {
+    btn.disabled = false;
+    const container = document.getElementById("container");
+    btn.onclick = () => {
+      container.style.display = "block";
+      display(result.id);
+      const papers = document.querySelector(".papers");
+      papers.style.opacity = "0.6";
+    };
+  }
+  li.appendChild(btn);
+  ul.appendChild(li);
 };
 fetchuploadfiles();
 
 cancel.onclick = () => {
-    container.style.display = 'none'
-    papers.style.opacity = '1'
-    result3 = document.getElementById('result3')
-    result3.style.display = 'block'
-    setTimeout(() => {
-        result3.style.display = 'none'
-    }, 2000)
-    const papers = document.querySelector('.papers')
-    papers.style.opacity = '0.6'
-}
-
+  container.style.display = "none";
+  papers.style.opacity = "1";
+  result3 = document.getElementById("result3");
+  result3.style.display = "block";
+  setTimeout(() => {
+    result3.style.display = "none";
+  }, 2000);
+  const papers = document.querySelector(".papers");
+  papers.style.opacity = "0.6";
+};
 
 const display = async (id) => {
-    const path = document.getElementById('path')
-    const sno = document.getElementById('paperid')
-    const name = document.getElementById('name')
-    const email = document.getElementById('email')
-    const col_name = document.getElementById('col_name')
-    const token = localStorage.getItem('accessToken');
-    const headers = new Headers();
-    headers.append('Authorization', `Bearer ${token}`);
-    const response = await fetch(`/api/papers_detail?id=${id}`, {
-        headers: {
-            'Content-type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-        },
-    })
-    if (response.ok) {
-        const details = await response.json()
-        const a = document.createElement('a')
-        a.href = details.filepath
-        a.textContent = "View the file"
-        path.appendChild(a)
-        name.textContent = details.name
-        email.textContent = details.email
-        sno.textContent = details.sno
-        col_name.textContent = details.col_name
-        const btn = document.getElementById('approve')
-        btn.onclick = () => {
-            fac_allotment(details.sno)
-        }
-    }
-}
+  const path = document.getElementById("path");
+  const sno = document.getElementById("paperid");
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const col_name = document.getElementById("col_name");
+  const token = localStorage.getItem("accessToken");
+  const headers = new Headers();
+  headers.append("Authorization", `Bearer ${token}`);
+  const response = await fetch(`/api/papers_detail?id=${id}`, {
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  if (response.ok) {
+    const details = await response.json();
+    const a = document.createElement("a");
+    a.href = details.filepath;
+    a.textContent = "View the file";
+    a.target = "_blank";
+    path.appendChild(a);
+    name.textContent = details.name;
+    email.textContent = details.email;
+    sno.textContent = details.sno;
+    col_name.textContent = details.col_name;
+    const btn = document.getElementById("approve");
+    btn.onclick = () => {
+      fac_allotment(details.sno);
+    };
+  }
+};
 
-// allotment code for faculty 
+// allotment code for faculty
 const fac_allotment = async (id) => {
-    const email = document.getElementById('fac_mail').value
-    console.log(email)
-    const response = fetch(`http://localhost:3000/paper_allot?id=${id}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email })
-    })
-    if (response.status = 200) {
-        const result1 = document.getElementById('result1')
-        result1.style.display = 'block'
-        const container = document.getElementById('container')
-        container.style.display = 'none'
-        setTimeout(() => {
-            result1.style.display = 'none'
-        }, 2000)
-        const papers = document.querySelector('.papers')
-        papers.style.opacity = '0.6'
-    }
-    else {
-        const result2 = document.getElementById('result2')
-        result2.style.display = 'block'
-        const container = document.getElementById('container')
-        container.style.display = 'none'
-        setTimeout(() => {
-            result2.style.display = 'none'
-        }, 2000)
-        const papers = document.querySelector('.papers')
-        papers.style.opacity = '0.6'
-    }
-}
+  const email = document.getElementById("fac_mail").value;
+  console.log(email);
+  const response = fetch(`http://localhost:3000/paper_allot?id=${id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email }),
+  });
+  if ((response.status = 200)) {
+    const result1 = document.getElementById("result1");
+    result1.style.display = "block";
+    const container = document.getElementById("container");
+    container.style.display = "none";
+    setTimeout(() => {
+      result1.style.display = "none";
+    }, 2000);
+    const papers = document.querySelector(".papers");
+    papers.style.opacity = "0.6";
+  } else {
+    const result2 = document.getElementById("result2");
+    result2.style.display = "block";
+    const container = document.getElementById("container");
+    container.style.display = "none";
+    setTimeout(() => {
+      result2.style.display = "none";
+    }, 2000);
+    const papers = document.querySelector(".papers");
+    papers.style.opacity = "0.6";
+  }
+};
