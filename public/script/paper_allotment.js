eef83baff4285aa71fb8c22b62b3bbbadd956cfc
gowtham <68599050+ygowthamr@users.noreply.github.com>
2024-10-11 11:48:04 +0530

    Update paper_allotment.js for viewing older files also

--- public/script/paper_allotment.js
+++ public/script/paper_allotment.js
@@ -92,7 +92,7 @@ const display = async (id) => {
       a.target = "_blank";
       path.appendChild(a);
     }
-    a.href = details.filepath;
+    a.href = `/uploads/${details.filename}`;
     a.textContent = "View the file";
 
     name.textContent = details.name;
