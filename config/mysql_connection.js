32259621197821bd1222f07b6816481684fc3fe5
smog-root <machha.shashank@gmail.com>
2024-10-16 22:07:26 +0530

    Add_MySQL_Connection_Pool_with_Environment_Variable_Support

--- config/mysql_connection.js
+++ config/mysql_connection.js
@@ -1,11 +1,11 @@
-const mysql=require('mysql')
+const mysql = require('mysql');
+require('dotenv').config();
 
-require("dotenv").config()
-const DB_HOST = process.env.DB_HOST
-const DB_USER = process.env.DB_USER
-const DB_PASSWORD = process.env.DB_PASSWORD
-const DB_DATABASE = process.env.DB_DATABASE
-const DB_PORT = process.env.DB_PORT
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PORT = process.env.DB_PORT;
 
 const db = mysql.createPool({
     connectionLimit: 100,
@@ -14,6 +14,25 @@ const db = mysql.createPool({
     password: DB_PASSWORD,
     database: DB_DATABASE,
     port: DB_PORT
-})
+});
 
-module.exports = db
+// Check connection
+db.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.');
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.');
+        }
+    }
+
+    if (connection) connection.release();
+
+    return;
+});
+
+module.exports = db;
