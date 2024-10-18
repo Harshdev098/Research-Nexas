82f873781aefbaed3facbcdb27eaa022f6957843
trinetra110 <ayanmondal1805@gmail.com>
2024-10-18 13:33:28 +0530

    fix: size of dark-theme icon in stk_mainpage.html and changed icons in stk_mainpage.js for better visibility

--- public/script/stk_mainpage.js
+++ public/script/stk_mainpage.js
@@ -2,9 +2,9 @@ var icon = document.getElementById("icon");
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "images/sun.png";
+    icon.src = "images/dark_mode.webp";
   } else {
-    icon.src = "images/moon.png";
+    icon.src = "images/moon.webp";
   }
 };
 document.addEventListener("DOMContentLoaded", function () {
