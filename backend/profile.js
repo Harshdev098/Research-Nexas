aa16ff92f63a2924f898bb5fa4a5e23f7c5387af
ygowthamr <ygowthamr@gmail.com>
2024-10-20 08:49:08 +0530

    Updated the required changes

--- backend/profile.js
+++ backend/profile.js
@@ -2,8 +2,6 @@ const {decodeAccessToken}=require('../login-system/token')
 const  db  = require('../config/mysql_connection')
 const mysql=require('mysql')
 
-let UserProfileID="";
-
 const display=async(req,res)=>{
     const decodedtoken = decodeAccessToken(req.headers.authorization);
     if (!decodedtoken || !decodedtoken.user) {
@@ -17,7 +15,6 @@ const display=async(req,res)=>{
         const query=mysql.format(sqlquery,[userid])
         await connection.query(query,(err,result)=>{
             if(err) throw err;
-            UserProfileID = result[0].id;
             const username=result[0].username
             const name=result[0].name;
             const email=result[0].email;
@@ -25,7 +22,6 @@ const display=async(req,res)=>{
             const state=result[0].state;
             const year=result[0].year;
             const course=result[0].course;
-            // console.log(name,email,col_name,state,year,course)
             res.status(200).json({username,name,email,col_name,state,year,course});
             connection.release();
         })
@@ -33,9 +29,16 @@ const display=async(req,res)=>{
 }
 
 const updateProfile = async (req, res) => {
+    const decodedtoken = decodeAccessToken(req.headers.authorization);
+    if (!decodedtoken || !decodedtoken.user) {
+        console.error('Invalid or missing user information in the token');
+        return res.status(401).send('Unauthorized');
+    }
+    const userid=decodedtoken.user;
+
     const { col_name, state, year, course } = req.body;
 
-    if (!UserProfileID) {
+    if (!userid) {
         return res.status(400).json({ error: 'User ID is required.' });
     }
 
@@ -45,7 +48,7 @@ const updateProfile = async (req, res) => {
       SET col_name = ?, state = ?, year = ?, course = ? 
       WHERE id = ?
     `;
-    const infoValues = [col_name, state, year, course, UserProfileID];
+    const infoValues = [col_name, state, year, course, userid];
 
     db.query(infoQuery, infoValues, (err, infoResult) => {
         if (err) {
